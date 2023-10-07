f1874a1d768daae71c0545f76d19ce129f74d311
cheeeese.24 <cheeeese.24@gmail.com>
2023-10-07 16:15:17 +0900

    java起動だけできる状態

---
+++ src/test/java/com/kensuke/KensukeSandBox/KensukeSandBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kensuke.KensukeSandBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KensukeSandBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
