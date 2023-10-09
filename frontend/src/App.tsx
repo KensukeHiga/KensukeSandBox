f71eed8335b5452303339101c5765128d38f2149
cheeeese.24 <cheeeese.24@gmail.com>
2023-10-09 15:56:20 +0900

    機能入れるための画面完了
    スタート時の自動生成された残骸がまだ残っている。。。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,68 @@
 import React from "react";
-import { Button, Container, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Grid,
+  Typography,
+  ThemeProvider,
+  createTheme,
+  Box,
+} from "@mui/material";
+import logo from "./logo.svg";
 
 function App() {
   const featureList: string[] = ["TODO", "ユーザー情報", "GPT先生"];
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+    components: {
+      MuiContainer: {
+        styleOverrides: {
+          root: {
+            height: "100vh",
+            background: "linear-gradient(45deg, #2c3e50, #34495e)",
+            backgroundImage: `url(${logo})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "50%",
+            backgroundBlendMode: "overlay",
+          },
+        },
+      },
+    },
+  });
+
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        機能リスト画面
-      </Typography>
-      <Grid container spacing={3}>
-        {featureList.map((feature, index) => (
-          <Grid item key={index}>
-            <Button variant="contained" color="primary">
-              {feature}
-            </Button>
+    // sx={{background:"linear-gradient(45deg, rgba(0,216,255,1) 0%, rgba(77,110,243,1) 100%)"}} // Reactブルーの線形グラデーション
+    // sx={{ background: "linear-gradient(45deg, #2c3e50, #34495e)" }} // ダークな色合い
+    <Box sx={{ background: "linear-gradient(45deg, #2c3e50, #34495e)" }}>
+      <ThemeProvider theme={darkTheme}>
+        <Container>
+          <Typography
+            variant="h4"
+            style={{
+              color: "#E0F2F1", // ホワイトの代わりにシアンの薄い色
+              fontWeight: "bold",
+              marginBottom: "16px", // タイトルとボタンの間にスペースを持たせる
+              textShadow: "4px 4px 4px rgba(0, 0, 0, 0.2)", // ソフトな文字の影
+            }}
+          >
+            機能リスト画面
+          </Typography>
+          <Grid container spacing={3}>
+            {featureList.map((feature, index) => (
+              <Grid item key={index}>
+                <Button variant="contained" color="primary">
+                  {feature}
+                </Button>
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-    </Container>
+        </Container>
+      </ThemeProvider>
+    </Box>
   );
 }
 
