b929fc2cbb4e8615a37e7895a6a9ac2e79fec6f4
OFFICE_Kensuke <cheeeese.24@gmail.com>
2023-10-18 11:32:02 +0900

    API受信（バック）

--- src/main/java/com/suke/kensukesandbox/controller/HelloWorldController.java
+++ src/main/java/com/suke/kensukesandbox/controller/HelloWorldController.java
@@ -1,12 +1,32 @@
 package com.suke.kensukesandbox.controller;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/todos")
 public class HelloWorldController {
+    @PostMapping
+    public String postTodo(
+            @RequestBody TodoFromRequest req
+    ) {
+
+        return "TOMATO";
+    }
+
     @GetMapping("/hello")
     public String helloWorld() {
         return "Hello, world";
+    }
+
+
+    @Data
+    public static class TodoFromRequest {
+        private String name;
+        private String content;
     }
 }
